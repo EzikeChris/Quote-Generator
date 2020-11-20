5241c1455cbde3c8c2e176651b847b93fc137633
ezikechris <christophermbe1009@gmail.com>
2020-11-20 01:58:01 +0100

    add functions

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn= document.getElementById('new-quote');
+
+
 // Get Qoute From Api
 
 async function getQuote() {
@@ -5,13 +12,14 @@ async function getQuote() {
  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 try {
     const response = await fetch(proxyUrl + apiUrl);
-    const data = await response.json();
-    console.log(data);
-} catch (error) {
+    const data = await response.json()
+    authorText.innerText = data.quoteAuthor;
+    quoteText.innerText = data.quoteText;
+ } catch (error) {
     getQuote();
-    console.log('whoops, Missing Quotes', error);
-    
-}
+
+
+ }
 
 }
 
